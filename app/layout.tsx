a1377a4e0f0a278cd484e4a5db75ed3c07a2fec0
thammanit-web <thammanitrinthang@gmail.com>
2025-03-07 13:35:37 +0700

    update logo app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,21 @@
-import type { ReactNode } from 'react';
-import './globals.css';
+import "./globals.css";
+export const metadata={
+  title: 'จองคิวจอดรถ',
+  icons:{
+    icon:['/favicon.ico']
+  }
+}
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body className="min-h-screen bg-gray-100">{children}</body>
+      <body>        
+        {children}
+      </body>
     </html>
   );
 }
